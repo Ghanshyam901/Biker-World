fcd369981dd48e4faec183ed4e0f430602862c37
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-01-22 14:26:47 +0530

    final

---
+++ script.js
@@ -0,0 +1,13 @@
+var MenuBar = document.getElementById("Menuitems");
+Menuitems.style.maxHeight="0px";
+
+function menutoggle(){
+    if(Menuitems.style.maxHeight="0px"){
+
+    Menuitems.style.maxHeight="200px";
+    }else{
+        Menuitems.style.maxHeight="0px";
+    }
+}
+
+
